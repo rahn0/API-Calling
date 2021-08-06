0259d75ee3769d0ead1894b1ebe2278f8fba1a2e
rahn <ryanahn2000@gmail.com>
2021-08-05 18:49:38 -0700

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Ryan Ahn on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
