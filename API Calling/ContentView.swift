6d25bcebcb633e0d047e322e563a19fd0b0b7f2f
rahn <ryanahn2000@gmail.com>
2021-08-05 19:41:38 -0700

    Added app icon and completed project

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Ryan Ahn on 7/29/21.
 //
 
+// API Calling for elements of the periodic table
+
 import SwiftUI
 struct ContentView: View {
     @State private var elements = [Element]()
@@ -18,6 +20,10 @@ struct ContentView: View {
                             .padding()
                         Text(element.symbol)
                             .padding()
+                        Text(element.atomicNumber)
+                            .padding()
+                        Text(element.groupBlock)
+                            .padding()
                         Text(element.history)
                             .padding()
                         Text(element.facts)
@@ -30,11 +36,12 @@ struct ContentView: View {
                         }
                     })
             }
-            .navigationTitle("Periodic Elements")
+            .navigationTitle("Elements of the Periodic Table")
         }
         .onAppear(perform: {
             queryAPI()
         })
+        // Set up alert
         .alert(isPresented: $showingAlert, content: {
             Alert(title: Text("Loading Error"),
                   message: Text("There was a problem loading the data"),
@@ -52,10 +59,11 @@ struct ContentView: View {
                 for item in contents {
                     let name = item["name"].stringValue
                     let symbol = item["symbol"].stringValue
+                    let atomicNumber = item["atomicNumber"].stringValue
+                    let groupBlock = item["groupBlock"].stringValue
                     let facts = item["facts"].stringValue
                     let history = item["history"].stringValue
-                    let element = Element(symbol: symbol, name: name, history:
-                                            history, facts: facts)
+                    let element = Element(symbol: symbol, atomicNumber: atomicNumber, groupBlock: groupBlock, name: name, history: history, facts: facts)
                     elements.append(element)
                 }
                 return
@@ -74,6 +82,8 @@ struct ContentView_Previews: PreviewProvider {
 struct Element: Identifiable {
     let id = UUID()
     let symbol: String
+    let atomicNumber: String
+    let groupBlock: String
     let name: String
     let history: String
     let facts: String
